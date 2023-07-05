c4d5e6072de91fb16f2d03971f1c045eef1f5dfc
갸갸 <quietwave00@gmail.com>
2023-07-05 23:56:34 +0900

    feat: add corsConfig, securityConfig

---
+++ src/main/java/com/tagstory/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.tagstory.config;
+
+import com.tagstory.config.oauth.PrincipalOauth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CorsConfig corsConfig;
+    private final PrincipalOauth2UserService principalOauth2UserService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .apply(new CustomDsl())
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers("/api/**")
+//                .access("hasRole('ROLE_GUEST') or hasRole('ROLE_USER')")
+//                .antMatchers("/api/user/**")
+//                .access("hasRole('ROLE_USER')")
+//                .anyRequest().permitAll()
+                .and()
+                .oauth2Login()
+                .loginPage("/login")
+                .userInfoEndpoint()
+                .userService(principalOauth2UserService);
+
+
+        return http.build();
+    }
+
+    public class CustomDsl extends AbstractHttpConfigurer<CustomDsl, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
+            http
+                    .addFilter(corsConfig.corsFilter());
+        }
+    }
+}
+
