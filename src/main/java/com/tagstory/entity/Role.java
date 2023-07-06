4c2a12c0f75fa365d81024d6bcf5f4fd036d9ac0
갸갸 <quietwave00@gmail.com>
2023-07-06 23:59:41 +0900

    faet: add user entity, repository

---
+++ src/main/java/com/tagstory/entity/Role.java
@@ -0,0 +1,5 @@
+package com.tagstory.entity;
+
+public enum Role {
+    ROLE_GUEST, ROLE_USER
+}
---
+++ src/main/java/com/tagstory/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.tagstory.user.repository;
+
+import com.tagstory.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmailAndUserKey(String email, String userKey);
+}
