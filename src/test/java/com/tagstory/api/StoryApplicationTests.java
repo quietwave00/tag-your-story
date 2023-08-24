bc5705c3dc407218053a6c4a4f24d350df0ab0b0
갸갸 <quietwave00@gmail.com>
2023-08-24 21:39:24 +0900

    refacotr: yml 분리

--- src/test/java/com/tagstory/api/StoryApplicationTests.java
+++ src/test/java/com/tagstory/api/StoryApplicationTests.java
@@ -1,8 +1,10 @@
 package com.tagstory.api;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled("test 환경 구축 후 테스트")
 @SpringBootTest
 class StoryApplicationTests {
 
