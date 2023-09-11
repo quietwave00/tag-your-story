100b1a27770f93c29e9da1f60b332188cc6a2261
갸갸 <quietwave00@gmail.com>
2023-09-11 23:41:29 +0900

    refactor: 응답 필드 추가

--- src/main/java/com/tagstory/api/domain/board/dto/response/BoardByTrackResponse.java
+++ src/main/java/com/tagstory/api/domain/board/dto/response/BoardByTrackResponse.java
@@ -1,9 +1,11 @@
 package com.tagstory.api.domain.board.dto.response;
 
+import com.tagstory.core.domain.board.dto.response.BoardByTrack;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Builder
@@ -12,13 +14,15 @@ public class BoardByTrackResponse {
     private String content;
     private LocalDateTime createdAt;
     private String nickname;
+    private List<String> hashtagList;
 
-    public static BoardByTrackResponse create(com.tagstory.core.domain.board.dto.response.BoardByTrack boardByTrack) {
+    public static BoardByTrackResponse create(BoardByTrack boardByTrack) {
         return BoardByTrackResponse.builder()
                 .boardId(boardByTrack.getBoardId())
                 .content(boardByTrack.getContent())
                 .createdAt(boardByTrack.getCreatedAt())
                 .nickname(boardByTrack.getNickname())
+                .hashtagList(boardByTrack.getHashtagList())
                 .build();
     }
 
--- src/main/java/com/tagstory/core/domain/board/dto/response/BoardByTrack.java
+++ src/main/java/com/tagstory/core/domain/board/dto/response/BoardByTrack.java
@@ -1,10 +1,13 @@
 package com.tagstory.core.domain.board.dto.response;
 
 import com.tagstory.core.domain.board.BoardEntity;
+import com.tagstory.core.domain.hashtag.HashtagEntity;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Builder
@@ -13,6 +16,7 @@ public class BoardByTrack {
     private String content;
     private LocalDateTime createdAt;
     private String nickname;
+    private List<String> hashtagList;
 
     public static BoardByTrack onComplete(BoardEntity board) {
         return BoardByTrack.builder()
@@ -20,6 +24,7 @@ public class BoardByTrack {
                 .content(board.getContent())
                 .createdAt(board.getCreatedAt())
                 .nickname(board.getUser().getNickname())
+                .hashtagList(board.getHashtagList().stream().map(HashtagEntity::getName).collect(Collectors.toList()))
                 .build();
     }
 }
