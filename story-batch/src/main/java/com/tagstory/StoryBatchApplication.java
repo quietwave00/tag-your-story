db67b0f26b7863aaf124778ff9f7aa7ba6b64444
갸갸 <quietwave00@gmail.com>
2023-10-26 21:49:26 +0900

    update

---
+++ story-batch/src/main/java/com/tagstory/StoryBatchApplication.java
@@ -0,0 +1,10 @@
+package com.tagstory;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryBatchApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
