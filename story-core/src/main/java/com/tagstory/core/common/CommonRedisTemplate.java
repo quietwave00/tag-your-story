69ff68b8c9ae1016527c2719698cd4fdb52d6eb9
갸갸 <quietwave00@gmail.com>
2023-10-28 18:37:33 +0900

    feat: 공통 RedisTemplate에 리스트 타입의 데이터를 처리하는 메소드 추가

--- story-core/src/main/java/com/tagstory/core/common/CommonRedisTemplate.java
+++ story-core/src/main/java/com/tagstory/core/common/CommonRedisTemplate.java
@@ -2,14 +2,16 @@ package com.tagstory.core.common;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tagstory.core.config.CacheSpec;
 import com.tagstory.core.exception.CustomException;
 import com.tagstory.core.exception.ExceptionCode;
-import com.tagstory.core.config.CacheSpec;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -23,9 +25,23 @@ public class CommonRedisTemplate {
      */
     public <V> void set(Object id, V value, CacheSpec cacheSpec) {
         try {
-            String jsonString =  objectMapper.writeValueAsString(value);
-            redisTemplate.opsForValue().set(cacheSpec.generateKey(id), jsonString, cacheSpec.getTtl());
+            String json =  objectMapper.writeValueAsString(value);
+            redisTemplate.opsForValue().set(cacheSpec.generateKey(id), json, cacheSpec.getTtl());
         } catch (JsonProcessingException e) {
+            throw new CustomException(ExceptionCode.CONVERSION_EXCEPTION);
+        }
+    }
+
+    /*
+     * 리스트 타입의 데이터를 덮어씌우지 않고 레디스에 저장한다.
+     */
+    public <V> void setList(Object id, V value, CacheSpec cacheSpec) {
+        try {
+            String json = objectMapper.writeValueAsString(value);
+            String key = cacheSpec.generateKey(id);
+            redisTemplate.opsForList().leftPush(key, json);
+            redisTemplate.expire(key, cacheSpec.getTtl());
+        } catch(JsonProcessingException e) {
             throw new CustomException(ExceptionCode.CONVERSION_EXCEPTION);
         }
     }
@@ -44,6 +60,22 @@ public class CommonRedisTemplate {
         }
         return null;
     }
+
+    /*
+     * 리스트 타입의 데이터를 가져온다.
+     */
+    public <T> T getList(Object id, CacheSpec cacheSpec) {
+        List<String> jsonList = redisTemplate.opsForList().range(cacheSpec.generateKey(id), 0, -1);
+        if(jsonList.size() > 0) {
+            try {
+                return objectMapper.readValue(String.valueOf(jsonList), cacheSpec.getTypeReference());
+            } catch (JsonProcessingException e) {
+                throw new CustomException(ExceptionCode.CONVERSION_EXCEPTION);
+            }
+        }
+        return null;
+    }
+
 
     /*
      * 데이터를 레디스에서 지운다.
