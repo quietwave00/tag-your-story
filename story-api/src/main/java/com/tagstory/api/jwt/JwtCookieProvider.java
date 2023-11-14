1aa87d1ca58aa38fb11b2c926e20e5e0e737e94c
갸갸 <quietwave00@gmail.com>
2023-11-15 00:21:08 +0900

    #41 쿠키 생성 수정

--- story-api/src/main/java/com/tagstory/api/jwt/JwtCookieProvider.java
+++ story-api/src/main/java/com/tagstory/api/jwt/JwtCookieProvider.java
@@ -11,23 +11,31 @@ import static com.tagstory.core.utils.JwtProperties.*;
 @Component
 public class JwtCookieProvider {
 
-    public Cookie generateAccessTokenCookie(String jwt) {
-        Cookie accessCookie = new Cookie(HEADER_STRING, encodeJwt(jwt));
+    private static final String DOMAIN = "tagyourstory.blog";
+
+    public Cookie generateAccessTokenCookie(String accessToken) {
+        Cookie accessCookie = new Cookie(HEADER_STRING, encodeJwt(accessToken));
+        accessCookie.setSecure(true);
         accessCookie.setPath("/");
+        accessCookie.setDomain(DOMAIN);
         accessCookie.setMaxAge(10800);
         return accessCookie;
     }
 
     public Cookie generateRefreshTokenCookie(String refreshToken) {
         Cookie refreshCookie = new Cookie(TOKEN_TYPE_REFRESH, encodeJwt(refreshToken));
+        refreshCookie.setSecure(true);
         refreshCookie.setPath("/");
+        refreshCookie.setDomain(DOMAIN);
         refreshCookie.setMaxAge(86400);
         return refreshCookie;
     }
 
     public Cookie generatePendingUserCookie(String tempToken) {
         Cookie tempCookie = new Cookie(TOKEN_TYPE_TEMP, tempToken);
+        tempCookie.setSecure(true);
         tempCookie.setPath("/");
+        tempCookie.setDomain(DOMAIN);
         tempCookie.setMaxAge(10800);
         return tempCookie;
     }
