9a067b10bcff92716df442756e933a57a8a21ec9
nagool <quietwave00@gmail.com>
2023-11-20 20:01:43 +0900

    젠킨스 빌드 테스트

--- story-api/src/main/java/com/tagstory/StoryApplication.java
+++ story-api/src/main/java/com/tagstory/StoryApplication.java
@@ -1,12 +1,14 @@
 package com.tagstory;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class StoryApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StoryApplication.class, args);
+		log.info("Jenkins Test");
 	}
 }
