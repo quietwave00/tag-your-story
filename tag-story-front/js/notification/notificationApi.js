a17f2712453dddcfc8eba9d99d2fb2a24e9c26a1
quietwave00 <quietwave00@gmail.com>
2023-12-14 18:21:09 +0900

    feat: 클라이언트 알림 목록 조회, 페이징 구현

--- tag-story-front/js/notification/notificationApi.js
+++ tag-story-front/js/notification/notificationApi.js
@@ -1,5 +1,10 @@
+/**
+ * 알림 읽음 처리를 요청한다.
+ * 
+ * @param notificationId: 알림 아이디
+ */
 const setAsRead = (notificationId) => {
-    return fetch(`${server_host}/api/notification`,{
+    fetch(`${server_host}/api/notification`,{
         method:"PATCH",
         headers: {
             "Content-Type": "application/json",
@@ -20,4 +25,61 @@ const setAsRead = (notificationId) => {
     });
 }
 
+/**
+ * 알림 리스트 조회를 요청한다.
+ * 
+ * @param page: 알림 페이지 
+ * @returns 
+ */
+const getNotificationList = (page) => {
+    return fetch(`${server_host}/api/notification?page=${page}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": localStorage.getItem('Authorization')
+        }
+    })
+    .then((res) => res.json())
+    .then(res => {
+        if (res.success === true) {
+            return Promise.resolve(res.response);
+        } else {
+            ExceptionHandler.handleException(res.exceptionCode)
+                .then(() => {
+                    getNotificationList(page);
+                });
+        }
+    });
+}
+
+/**
+ * 로그인 유저에 해당하는 전체 알림 개수를 요청한다.
+ * 
+ * @returns Subscriber에 대한 전체 알림 개수
+ */
+const getNotificationCount = () => {
+    return fetch(`${server_host}/api/notification/count`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": localStorage.getItem('Authorization')
+        }
+    })
+    .then((res) => res.json())
+    .then(res => {
+        if (res.success === true) {
+            return Promise.resolve(res.response);
+        } else {
+            ExceptionHandler.handleException(res.exceptionCode)
+                .then(() => {
+                    getNotificationCount();
+                });
+        }
+    });
+}
+
 export { setAsRead };
+export default {
+    getNotificationList,
+    getNotificationCount
+}
