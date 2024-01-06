7aa86c7fe6dbc149b016b8065ec49199f0ade87f
nagool <quietwave00@gmail.com>
2024-01-06 16:14:44 +0900

    refactor: 일급컬렉션 응답 시 @JsonUnwrapped 적용

--- story-api/src/main/java/com/tagstory/api/domain/tracks/dto/response/RankingListResponse.java
+++ story-api/src/main/java/com/tagstory/api/domain/tracks/dto/response/RankingListResponse.java
@@ -1,5 +1,6 @@
 package com.tagstory.api.domain.tracks.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import com.tagstory.core.domain.tracks.service.dto.response.RankingList;
 import lombok.Builder;
 import lombok.Getter;
@@ -7,7 +8,7 @@ import lombok.Getter;
 @Getter
 @Builder
 public class RankingListResponse {
-    // @TODO
+    @JsonUnwrapped
     RankingList rankingList;
 
     public static RankingListResponse from(RankingList rankingList) {
