652cbaa0963dc6e33d9061cd3dc502a35c73c98f
nagool <quietwave00@gmail.com>
2024-02-11 15:13:24 +0900

    레디스 비밀번호 설정

--- story-core/src/main/java/com/tagstory/core/config/RedisConfig.java
+++ story-core/src/main/java/com/tagstory/core/config/RedisConfig.java
@@ -6,6 +6,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 
@@ -13,13 +14,21 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 @EnableCaching
 @RequiredArgsConstructor
 public class RedisConfig {
-    @Value("${spring.redis.host}")
+    @Value("${spring.data.redis.host}")
     private String host;
-    @Value("${spring.redis.port}")
+
+    @Value("${spring.data.redis.port}")
     private int port;
+
+    @Value("${spring.data.redis.password}")
+    private String password;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        RedisStandaloneConfiguration connectionFactory = new RedisStandaloneConfiguration();
+        connectionFactory.setHostName(host);
+        connectionFactory.setPort(port);
+        connectionFactory.setPassword(password);
+        return new LettuceConnectionFactory(connectionFactory);
     }
 }
--- story-core/src/main/java/com/tagstory/core/config/RedissonConfig.java
+++ story-core/src/main/java/com/tagstory/core/config/RedissonConfig.java
@@ -3,24 +3,30 @@ package com.tagstory.core.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.redisson.config.SingleServerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedissonConfig {
-    @Value("${spring.redis.host}")
+    @Value("${spring.data.redis.host}")
     private String redisHost;
 
-    @Value("${spring.redis.port}")
+    @Value("${spring.data.redis.port}")
     private int redisPort;
+
+    @Value("${spring.data.redis.password}")
+    private String password;
 
     private static final String REDISSON_HOST_PREFIX = "redis://";
 
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + redisHost + ":" + redisPort);
+        SingleServerConfig singleServerConfig = config.useSingleServer();
+        singleServerConfig.setAddress(REDISSON_HOST_PREFIX + redisHost + ":" + redisPort);
+        singleServerConfig.setPassword(password);
         return Redisson.create(config);
     }
 }
